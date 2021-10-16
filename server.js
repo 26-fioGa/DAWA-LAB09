b51c52f0cec8d2d5492a1a0ad47dc587f25cc1b4
fiorellaGamarra <fiorella.gamarra@tecsup.edu.pe>
2021-10-16 12:52:12 -0500

    heroku config 3

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/my-first-project'));
+app.use(express.static('/my-first-project'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/my-first-project/' }),
+    res.sendFile('index.html', { root: '/my-first-project' }),
 );
 
 // Start the app by listening on the default Heroku port
